818b55bf4533ff4af770b49b1b98bcd8cc50d548
Irfan hossain <32560686+toumalmojumder@users.noreply.github.com>
2020-03-18 15:58:48 +0600

    firebaseadded

--- app/src/main/java/com/leads/railwaytracker/MainActivity.java
+++ app/src/main/java/com/leads/railwaytracker/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineCallback;
 import com.mapbox.android.core.location.LocationEngineProvider;
@@ -15,6 +18,8 @@ import com.mapbox.android.core.location.LocationEngineRequest;
 import com.mapbox.android.core.location.LocationEngineResult;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
@@ -27,14 +32,19 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.light.Position;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements PermissionsListener  {
+public class MainActivity extends AppCompatActivity implements PermissionsListener {
 
     MapView mapview;
     MapboxMap mapboxMap;
@@ -44,12 +54,16 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
     private LocationChangeListeningActivityLocationCallback callback;
     private LocationEngine locationEngine;
     static int finalint = 1;
+    int gotvalue = 1;
     FirebaseDatabase database;
     DatabaseReference reference;
+    HashMap<String, String> detailmap;
+    Point originpoint;
+    Point destinationpoint;
+    ArrayList<String> names;
 
 
-
-
+    @SuppressLint("LogNotTimber")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,9 +72,56 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
         mapview = findViewById(R.id.mapview);
 
         database = FirebaseDatabase.getInstance();
-        reference = database.getReference();
-        
-        reference.child("stations").child("Gobra").setValue("22.947820,89.829226");
+        reference = database.getReference().child("stations");
+        detailmap = new HashMap<>();
+        names = new ArrayList<>();
+
+        reference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> iterable = dataSnapshot.getChildren();
+                for (DataSnapshot snapshot : iterable) {
+                    detailmap.put(snapshot.getKey(), snapshot.getValue().toString());
+                    names.add(snapshot.getKey());
+                }
+                Log.e("detailmap", detailmap.toString());
+
+                if (gotvalue == 1) {
+                    for (int i = 0; i < detailmap.size(); i++) {
+                        String[] latlng = detailmap.get(names.get(i)).split(",");
+                        destinationpoint = Point.fromLngLat(Double.parseDouble(latlng[0]), Double.parseDouble(latlng[1]));
+
+                        if (CalculateDistance(originpoint.latitude(), originpoint.longitude(), destinationpoint.latitude(), destinationpoint.longitude()) <= 5.0000) {
+                            Log.e("origin lat", String.valueOf(originpoint.latitude()));
+                            Log.e("origin long", String.valueOf(originpoint.longitude()));
+
+                            Log.e("destination lat", String.valueOf(destinationpoint.latitude()));
+                            Log.e("destination long", String.valueOf(destinationpoint.longitude()));
+
+
+                            GeoJsonSource source = new GeoJsonSource(String.valueOf(i),Feature.fromGeometry(destinationpoint));
+                            mapboxMap.getStyle().addSource(source);
+
+
+
+                            //add marker on map
+
+
+                            Log.e("distance", "you are close to " + names.get(i) + " Distance " + CalculateDistance(originpoint.latitude(), originpoint.longitude(), destinationpoint.latitude(), destinationpoint.longitude()));
+                        }
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
         mapview.onCreate(savedInstanceState);
@@ -71,8 +132,8 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-                        Toast.makeText(MainActivity.this, "MapLoaded Successfully", Toast.LENGTH_SHORT).show();
-                        callback = new LocationChangeListeningActivityLocationCallback(MainActivity.this,style);
+                        // Toast.makeText(MainActivity.this, "MapLoaded Successfully", Toast.LENGTH_SHORT).show();
+                        callback = new LocationChangeListeningActivityLocationCallback(MainActivity.this, style);
                         enableLocationComponent(style);
                     }
                 });
@@ -81,7 +142,26 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
     }
 
 
+    private double CalculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        return (dist);
+    }
 
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
 
 
     /**
@@ -161,13 +241,13 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
         }
     }
 
-    private static class LocationChangeListeningActivityLocationCallback
+    private class LocationChangeListeningActivityLocationCallback
             implements LocationEngineCallback<LocationEngineResult> {
 
         private final WeakReference<MainActivity> activityWeakReference;
         Style style;
 
-        LocationChangeListeningActivityLocationCallback(MainActivity activity,Style style) {
+        LocationChangeListeningActivityLocationCallback(MainActivity activity, Style style) {
             this.activityWeakReference = new WeakReference<>(activity);
             this.style = style;
         }
@@ -190,15 +270,18 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
                 }
                 // Create a Toast which displays the new location's coordinates
                 //Toast.makeText(activity,"new Location"+String.valueOf(result.getLastLocation().getLatitude())+String.valueOf(result.getLastLocation().getLongitude()),Toast.LENGTH_SHORT).show();
-                Log.e("new Location",String.valueOf(result.getLastLocation().getLatitude())+ result.getLastLocation().getLongitude());
+                Log.e("new Location", String.valueOf(result.getLastLocation().getLatitude()) + result.getLastLocation().getLongitude());
                 //activity.databaseReference.child("loc").setValue(result.getLastLocation().getLatitude()+","+result.getLastLocation().getLongitude());
                 // Pass the new location to the Maps SDK's LocationComponent
 
 
+                if (finalint == 1) {
+                    originpoint = Point.fromLngLat(result.getLastLocation().getLatitude(), result.getLastLocation().getLongitude());
+                    Log.e("origin latitude", String.valueOf(originpoint.latitude()));
+                    Log.e("origin longitude", String.valueOf(originpoint.longitude()));
 
-                if(finalint == 1){
                     CameraPosition position = new CameraPosition.Builder()
-                            .target(new LatLng(result.getLastLocation().getLatitude(),result.getLastLocation().getLongitude())) // Sets the new camera position
+                            .target(new LatLng(result.getLastLocation().getLatitude(), result.getLastLocation().getLongitude())) // Sets the new camera position
                             .zoom(12) // Sets the zoom
                             .bearing(180) // Rotate the camera
                             .tilt(30) // Set the camera tilt
@@ -208,15 +291,6 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
                             .newCameraPosition(position), 3000);
                     finalint++;
                 }
-
-
-
-
-
-
-
-
-
 
 
                 if (activity.mapboxMap != null && result.getLastLocation() != null) {
